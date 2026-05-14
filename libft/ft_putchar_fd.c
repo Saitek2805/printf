eca288c902cb8ee68c6eadc0e7675c439c83bf04
Keiner Hurtado <keinerhurtado2805@gmail.com>
2026-05-14 13:35:29 +0200

    Remove broken submodule and track libft normally

---
+++ libft/ft_putchar_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khurtado <khurtado@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 22:37:20 by khurtado          #+#    #+#             */
+/*   Updated: 2026/05/08 11:59:33 by khurtado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
+/*
+int	main(void)
+{
+
+	ft_putchar_fd('H', 1);
+	ft_putchar_fd('i', 1);
+	ft_putchar_fd('\n', 1);
+
+	return (0);
+}*/
